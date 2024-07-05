96afc4238e87d09f646b51d333fb1464ef992784
louiemarte10 <ldbopz17@gmail.com>
2024-07-06 01:05:44 +0700

    print payment list

--- src/pages/prints/payment-print-list.jsx
+++ src/pages/prints/payment-print-list.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
-import { Typography, Row, Col, Table, Form } from "antd";
+import React, { useRef } from "react";
+import ReactToPrint from "react-to-print";
+import { Typography, Table } from "antd";
 import CustomInput from "../../components/Input/Input";
+import CustomButton from "../../components/Button/Button"; // Assuming you have this component
 
-const { Title, Text } = Typography;
+const { Title } = Typography;
 
 const columns = [
   { title: "Name", dataIndex: "name", key: "name", width: 300 },
@@ -51,27 +53,47 @@ const dataSource = [
   },
 ];
 
-const PaymentPrintLIst = () => {
-  useEffect(() => {
-    window.print();
-  }, []);
+class PaymentPrintList extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="mx-auto p-5 font-sans">
+          <div className="text-center mb-5">
+            <Title level={3} className="text-center text-2xl font-bold">
+              PAYMENT LIST
+            </Title>
+          </div>
+
+          <Table
+            dataSource={dataSource}
+            columns={columns}
+            pagination={false}
+            bordered
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+const PrintComponent = () => {
+  const componentRef = useRef();
 
   return (
-    <div className="mx-auto p-5 font-sans">
-      <div className="text-center mb-5">
-        <Title level={3} className="text-center text-2xl font-bold">
-          PAYMENT LIST
-        </Title>
+    <div>
+      <PaymentPrintList ref={componentRef} />
+      <div className="text-right mb-5 w-fullflex justify-center">
+        <ReactToPrint
+          trigger={() => (
+            <CustomButton type="primary" size="large">
+              Print
+            </CustomButton>
+          )}
+          content={() => componentRef.current}
+        />
       </div>
-
-      <Table
-        dataSource={dataSource}
-        columns={columns}
-        pagination={false}
-        bordered
-      />
     </div>
   );
 };
 
-export default PaymentPrintLIst;
+export default PrintComponent;
